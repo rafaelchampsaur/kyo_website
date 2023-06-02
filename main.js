e88b20522b29c6efa37cfeaa52158af657844a92
rafaelchampsaur <39336390+rafaelchampsaur@users.noreply.github.com>
2023-06-02 20:44:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+// main.js
+const menuItems = [
+    { text: 'How We Teach', url: '#' },
+    { text: 'Contact Us', url: '#' },
+    { text: 'Prices', url: '#' },
+    { text: 'Availability', url: '#' },
+  ];
+  
+  const menu = document.getElementById('menu');
+  
+  function createMenu() {
+    const ul = document.createElement('ul');
+    menu.appendChild(ul);
+  
+    menuItems.forEach((menuItem) => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = menuItem.url;
+      a.textContent = menuItem.text;
+      li.appendChild(a);
+      ul.appendChild(li);
+    });
+  }
+  
+  function createVideoBackground() {
+    const videoBackground = document.createElement('div');
+    videoBackground.classList.add('video-background');
+  
+    const video = document.createElement('video');
+    video.setAttribute('autoplay', '');
+    video.setAttribute('loop', '');
+    video.setAttribute('muted', '');
+  
+    const source = document.createElement('source');
+    source.setAttribute('src', '/video.mp4');
+    source.setAttribute('type', '/video/mp4');
+  
+    video.appendChild(source);
+    videoBackground.appendChild(video);
+  
+    document.body.appendChild(videoBackground);
+  }
+  
+  createMenu();
+  createVideoBackground();
+  
+  
